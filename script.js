fdf2f13c243a36bed929678563b2d2fb903a276d
Marc Diallo <mark.devguru@outlook.com>
2025-03-05 21:56:06 -0600

    uses the data inside the HTML template and inserts it using the insertAdjacentHTML() method

--- script.js
+++ script.js
@@ -18,7 +18,22 @@ request.send();
 // console.log(request.responseText);
 
 request.addEventListener('load', function () {
-  console.log(this);
-  const data = JSON.parse(this.responseText);
+  const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = ` 
+<article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+          </div>
+        </article>
+`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
