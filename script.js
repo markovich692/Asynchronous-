b15fae4e0fe91d2b3b6cbe482f28b37cd0c745c5
Marc Diallo <mark.devguru@outlook.com>
2025-03-07 13:11:37 -0600

    handles the response.json() using by chaining a another then() mrthod, stores the data into the data1 variable and calls the renderCountry passing the data1 variable

--- script.js
+++ script.js
@@ -41,22 +41,22 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('cameroon');
 // getCountryData('Kazakhstan');
 
-// const renderCountry = function (data, className = '') {
-//   const html = `
-//     <article class= "country ${className}">
-//       <img class="country__img" src="${data.flag}" />
-//       <div class="country__data">
-//         <h3 class="country__name">${data.name}</h3>
-//         <h4 class="country__region">${data.region}</h4>
-//         <p class="country__row"><span>👫</span>${data.population}</p>
-//         <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//         <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-//       </div>
-//     </article>
-// `;
-//   countriesContainer.insertAdjacentHTML('beforeend', html);
-//   countriesContainer.style.opacity = 1;
-// };
+const renderCountry = function (data, className = '') {
+  const html = `
+    <article class= "country ${className}">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${data.population}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
 
 // ///////////////////////////////////////////////////////////////////////
 
@@ -107,16 +107,20 @@ const countriesContainer = document.querySelector('.countries');
 
 // getCountryDataAndBorder('Portugal');
 
-const request = fetch('https://restcountries.com/v2/name/portugal');
-console.log(request);
+// const request = fetch('https://restcountries.com/v2/name/portugal');
+// console.log(request);
 
 const getCountryData = function (country) {
   //Assuming the rresponse is fulfilled
-  fetch(`https://restcountries.com/v2/name/${country}`).then(function (
-    response
-  ) {
-    return response.json();
-  });
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(function (response) {
+      // console.log(response.json());
+      return response.json();
+    })
+    .then(function (data) {
+      const [data1] = data;
+      renderCountry(data1);
+    });
 };
 
-getCountryData(portugal);
+getCountryData('portugal');
