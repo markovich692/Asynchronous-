a72463d48f5d7a5e72f4163a08fe6fc9475c456f
Marc Diallo <mark.devguru@outlook.com>
2025-03-06 21:51:40 -0600

    renders Portugal and its neighbor Spain

--- script.js
+++ script.js
@@ -41,33 +41,51 @@ const countriesContainer = document.querySelector('.countries');
 // getCountryData('cameroon');
 // getCountryData('Kazakhstan');
 
+const renderCountry = function (data) {
+  const html = `
+    <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${data.population}</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>
+`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+///////////////////////////////////////////////////////////////////////
+
 const getCountryDataAndBorder = function (country) {
+  //AJAX call 1
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v2/name/${country}`);
   request.send();
 
-  // console.log(request.responseText);
-
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
+    console.log(data);
 
-    const html = `
-              <article class="country">
-                <img class="country__img" src="${data.flag}" />
-                <div class="country__data">
-                  <h3 class="country__name">${data.name}</h3>
-                  <h4 class="country__region">${data.region}</h4>
-                  <p class="country__row"><span>👫</span>${data.population}</p>
-                  <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-                  <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-                </div>
-              </article>
-      `;
+    //Render country 1
+    renderCountry(data);
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
+    //Get nieghbor Country 2
+    const neighbor = data?.borders[0];
+
+    //AJAX call 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbor}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      renderCountry(data2);
+    });
   });
 };
 
-getCountryData('portugal');
-// getCountryData('Kazakhstan');
+getCountryDataAndBorder('Portugal');
