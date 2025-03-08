c19ca91509446dc370135b882ccd19cbbae1d84e
Marc Diallo <mark.devguru@outlook.com>
2025-03-08 10:48:33 -0600

    creates the renderError() function and passes in the error message

--- script.js
+++ script.js
@@ -3,6 +3,59 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+
+  countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = '') {
+  const html = `
+      <article class= "country ${className}">
+        <img class="country__img" src="${data.flag}" />
+        <div class="country__data">
+          <h3 class="country__name">${data.name}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${data.population}</p>
+          <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+          <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+        </div>
+      </article>
+  `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  //Assuming the response is fulfilled
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      const [data1] = data;
+
+      //Gets the neighbor country from the data1
+      const neighbor = data1.borders?.[0];
+      renderCountry(data1);
+
+      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
+    })
+    .then(function (response1) {
+      return response1.json();
+    })
+    .then(function (data2) {
+      renderCountry(data2, 'neighbour');
+    })
+    .catch(error => {
+      console.error(error);
+
+      renderError(`Something went wrong ${error.message} Try again`);
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('portugal');
+});
+
 // NEW COUNTRIES API URL (use instead of the URL shown in videos):
 // https://restcountries.com/v2/name/portugal
 
@@ -40,25 +93,6 @@ const countriesContainer = document.querySelector('.countries');
 
 // getCountryData('cameroon');
 // getCountryData('Kazakhstan');
-
-const renderCountry = function (data, className = '') {
-  const html = `
-    <article class= "country ${className}">
-      <img class="country__img" src="${data.flag}" />
-      <div class="country__data">
-        <h3 class="country__name">${data.name}</h3>
-        <h4 class="country__region">${data.region}</h4>
-        <p class="country__row"><span>👫</span>${data.population}</p>
-        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-      </div>
-    </article>
-`;
-  countriesContainer.insertAdjacentHTML('beforeend', html);
-  countriesContainer.style.opacity = 1;
-};
-
-// ///////////////////////////////////////////////////////////////////////
 
 // const getCountryDataAndBorder = function (country) {
 //   //AJAX call 1
@@ -109,29 +143,3 @@ const renderCountry = function (data, className = '') {
 
 // const request = fetch('https://restcountries.com/v2/name/portugal');
 // console.log(request);
-
-const getCountryData = function (country) {
-  //Assuming the response is fulfilled
-  fetch(`https://restcountries.com/v2/name/${country}`)
-    .then(response => response.json())
-    .then(data => {
-      const [data1] = data;
-
-      //Gets the neighbor country from the data1
-      const neighbor = data1.borders?.[0];
-      renderCountry(data1);
-
-      return fetch(`https://restcountries.com/v2/alpha/${neighbor}`);
-    })
-    .then(function (response1) {
-      return response1.json();
-    })
-    .then(function (data2) {
-      renderCountry(data2, 'neighbour');
-    })
-    .catch(error => console.log(error));
-};
-
-btn.addEventListener('click', function () {
-  getCountryData('portugal');
-});
