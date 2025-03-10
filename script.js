066550f805ddabd605d187222f89dd446eb5ecf9
Marc Diallo <mark.devguru@outlook.com>
2025-03-10 17:12:27 -0500

    adds another Promise.resolve() along with a for loop to delay the response

--- script.js
+++ script.js
@@ -217,4 +217,10 @@ console.log(`let's start`);
 
 setTimeout(() => console.log(`0 sec timer`), 0);
 
-Promise.resolve('Resolved promise').then(res => console.log(res));
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+
+  console.log(res);
+});
